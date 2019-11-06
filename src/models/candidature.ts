35fb3cd308c8440c85c7427ef854cd51bc433211
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-07 00:04:43 +0100

    Add all candidature routes & PG db

---
+++ src/models/candidature.ts
@@ -0,0 +1,36 @@
+import db from '../config/database';
+import { Model, DataTypes } from 'sequelize';
+
+class Candidature extends Model {
+  private id!: number;
+  public fname!: string;
+  public lname!: string;
+  public familySituation!: string;
+  public birthdate!: Date;
+  public email!: string;
+  public phone!: string;
+}
+
+Candidature.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fname: DataTypes.STRING,
+    lname: DataTypes.STRING,
+    familySituation: DataTypes.STRING,
+    birthdate: DataTypes.DATE,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+  },
+  {
+    tableName: 'Candidature',
+    timestamps: true,
+    freezeTableName: true,
+    sequelize: db
+  }
+);
+
+export = Candidature;
--- src/routes/candidature.ts
+++ src/routes/candidature.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from 'express';
+import candidatureController from '../controllers/candidatures';
+import Candidature from '../models/candidature';
 const candidatureRouter = Router();
 
-candidatureRouter.get('/', (req: Request, res: Response) => {
-  res
-    .status(200)
-    .send('get all candidature');
+candidatureRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    const candidatures: Candidature[] = await candidatureController.getAll();
+    res.status(200).json(candidatures);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 
-export = candidatureRouter
+export = candidatureRouter;
