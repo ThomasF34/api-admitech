80da8495d7ba57589ca0207b7e5bfe72808beb98
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-08 15:30:46 +0100

    Fix sequelize error

---
+++ src/database/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+  staging: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  }
+};
+
--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import dbConfig from './config';
+const dbConfig = require('./config');
 
 
 const env = process.env.NODE_ENV || 'development';
