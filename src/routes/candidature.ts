28b57340e0a3263b11802343cd43e9d4d5d204fe
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-08 14:32:18 +0100

    Fix lint errors

--- src/routes/candidature.ts
+++ src/routes/candidature.ts
@@ -14,9 +14,9 @@ candidatureRouter.get('/', async (req: Request, res: Response) => {
 
 candidatureRouter.get('/:id', async (req: Request, res: Response) => {
   try {
-    const id: number = parseInt(req.params.id)
+    const id: number = parseInt(req.params.id);
     const candidature: Candidature = await candidatureController.getById(id);
-    if (candidature === null) res.status(404).json(`Candidature ${id} not found`)
+    if (candidature === null) res.status(404).json(`Candidature ${id} not found`);
     else res.status(200).json(candidature);
   } catch (e) {
     res.status(500).json(e.message);
