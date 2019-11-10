e1709f2ed301e043f73cef31e78d3fb11992785f
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-10 14:49:39 +0100

    Add Candidature sctructure w/o link to other object

--- src/database/seeders/20191107134412-candidature-fixture.js
+++ src/database/seeders/20191107134412-candidature-fixture.js
@@ -2,47 +2,56 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Candidature', [{
-      fname: 'Thomas',
-      lname: 'Falcone',
-      familySituation: 'Marié',
-      birthdate: new Date(),
-      email: 'monemail@email.com',
-      phone: '0000000000',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      fname: 'Lucas',
-      lname: 'Gonçalves',
-      familySituation: 'Marié',
-      birthdate: new Date(),
-      email: 'monemail2@email.com',
-      phone: '0000000001',
+      first_name: 'Thomas',
+      last_name: 'Falcone',
+      nationnality: 'Français',
+      birth_date: new Date(),
+      birth_place: 'montpellier',
+      family_status: 'single',
+      address: 'somewhere',
+      postal_code: '34000',
+      city: 'montpellier',
+      state: 'france',
+      bac_name: 'un bac',
+      bac_year: 2009,
+      bac_mention: 'bien',
+      bac_realname: 'un super bac',
+      last_facility_name: 'un etablissement',
+      last_facility_address: 'somewhere',
+      last_facility_postal_code: '34000',
+      last_facility_city: 'montpellier',
+      last_facility_state: 'france',
+      native_lang_name: 'français',
+      first_lang_name: 'anglais',
+      second_lang_name: 'italien',
+      third_lang_name: null,
+      first_lang_level: 'great',
+      second_lang_level: 'great',
+      third_lang_level: null,
+      internships: 'Franchement j\'ai fait des stages trop bien',
+      travels: 'j\'ai aussi voyagé',
+      it_knowledge: 'bon là j\'ai plus d\'inspi par contre',
+      sports_interests: 'du sport ? ',
+      strengths: 'mhhhh....',
+      other_apply: false,
+      other_apply_name: null,
+      other_apply_place: null,
+      other_apply_apprentise: null,
+      candidate_comment: 'je suis content ',
+      admin_comment: 'eleve a rejeter',
+      status: 'REFUSE',
+      branch: 'do',
+      certified: true,
+      certified_at: new Date(),
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
     return queryInterface.bulkDelete('Candidature', [{
-      email: 'monemail@email.com',
+      first_name: 'Thomas',
     }]);
   }
 };
--- src/models/candidature.ts
+++ src/models/candidature.ts
@@ -2,12 +2,50 @@ import db from '../database/config/database';
 import { Model, DataTypes } from 'sequelize';
 class Candidature extends Model {
   private id!: number;
-  public fname!: string;
-  public lname!: string;
-  public familySituation!: string;
-  public birthdate!: Date;
-  public email!: string;
   public phone!: string;
+  public first_name!: string;
+  public last_name!: string;
+  public nationnality!: string;
+  public birth_date!: Date;
+  public birth_place!: string;
+  public family_status!: string;
+  public address!: string;
+  public postal_code!: string;
+  public city!: string;
+  public state!: string;
+  public bac_name!: string;
+  public bac_year!: Date;
+  public bac_mention!: string;
+  public bac_realname!: string;
+  public last_facility_name!: string;
+  public last_facility_address!: string;
+  public last_facility_postal_code!: string;
+  public last_facility_city!: string;
+  public last_facility_state!: string;
+  public native_lang_name!: string;
+  public first_lang_name!: string;
+  public second_lang_name!: string;
+  public third_lang_name!: string;
+  public first_lang_level!: string;
+  public second_lang_level!: string;
+  public third_lang_level!: string;
+  public internships!: string;
+  public travels!: string;
+  public it_knowledge!: string;
+  public sports_interests!: string;
+  public strengths!: string;
+  public other_apply!: boolean;
+  public other_apply_name!: string;
+  public other_apply_place!: string;
+  public other_apply_apprentise!: boolean;
+  public candidate_comment!: string;
+  public admin_comment!: string;
+  public status!: string;
+  public branch!: string;
+  public certified!: boolean;
+  public certified_at!: Date;
+  public created_at!: Date;
+  public updated_at!: Date
 }
 
 Candidature.init({
@@ -16,12 +54,50 @@ Candidature.init({
     primaryKey: true,
     autoIncrement: true
   },
-  fname: DataTypes.STRING,
-  lname: DataTypes.STRING,
-  familySituation: DataTypes.STRING,
-  birthdate: DataTypes.DATE,
-  email: DataTypes.STRING,
-  phone: DataTypes.STRING
+  phone: DataTypes.STRING,
+  first_name: DataTypes.STRING,
+  last_name: DataTypes.STRING,
+  nationnality: DataTypes.STRING,
+  birth_date: DataTypes.DATEONLY,
+  birth_place: DataTypes.STRING,
+  family_status: DataTypes.STRING,
+  address: DataTypes.STRING,
+  postal_code: DataTypes.STRING,
+  city: DataTypes.STRING,
+  state: DataTypes.STRING,
+  bac_name: DataTypes.STRING,
+  bac_year: DataTypes.INTEGER,
+  bac_mention: DataTypes.STRING,
+  bac_realname: DataTypes.STRING,
+  last_facility_name: DataTypes.STRING,
+  last_facility_address: DataTypes.STRING,
+  last_facility_postal_code: DataTypes.STRING,
+  last_facility_city: DataTypes.STRING,
+  last_facility_state: DataTypes.STRING,
+  native_lang_name: DataTypes.STRING,
+  first_lang_name: DataTypes.STRING,
+  second_lang_name: DataTypes.STRING,
+  third_lang_name: DataTypes.STRING,
+  first_lang_level: DataTypes.STRING,
+  second_lang_level: DataTypes.STRING,
+  third_lang_level: DataTypes.STRING,
+  internships: DataTypes.TEXT,
+  travels: DataTypes.TEXT,
+  it_knowledge: DataTypes.TEXT,
+  sports_interests: DataTypes.TEXT,
+  strengths: DataTypes.TEXT,
+  other_apply: DataTypes.BOOLEAN,
+  other_apply_name: DataTypes.STRING,
+  other_apply_place: DataTypes.STRING,
+  other_apply_apprentise: DataTypes.BOOLEAN,
+  candidate_comment: DataTypes.TEXT,
+  admin_comment: DataTypes.TEXT,
+  status: DataTypes.STRING,
+  branch: DataTypes.STRING,
+  certified: DataTypes.BOOLEAN,
+  certified_at: DataTypes.DATE,
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE
 }, {
   timestamps: true,
   freezeTableName: true,
