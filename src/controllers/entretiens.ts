a2fee55d80397797e9134b5ec7fd80a39267834a
MartinCayuelas <martincayuelas@gmail.com>
2019-11-19 23:33:15 +0100

    [WIP] Set entretien to candidature

--- src/controllers/entretiens.ts
+++ src/controllers/entretiens.ts
@@ -9,4 +9,24 @@ function entretienByCandidature(idCanditature: number): Promise<Entretien> {
   });
 }
 
-export = { entretienByCandidature }
+function getEntretienById(idEntretien: number): Promise<Entretien> {
+  return Entretien.findOne({
+    where: {
+      id: idEntretien
+    }
+  });
+}
+
+function assignCandidatureToEntretien(idEntretien: number,idCanditature: number): Promise<[number, Entretien[]]> {
+  const elemToUpdate = {
+    candidature_id: idCanditature
+  };
+  return Entretien.update(elemToUpdate, {
+    where: {
+      id: idEntretien
+    }
+  });
+}
+
+
+export = { entretienByCandidature, getEntretienById, assignCandidatureToEntretien }
